1aae03d5479f72123cc808582f7bfde5d48073cb
PutthipongPOONBUA <putthipong.p@ku.th>
2019-02-23 23:22:18 +0700

    edit nav

--- lab4/index.php
+++ lab4/index.php
@@ -18,8 +18,21 @@
 </head>
 <body>
 
-<nav class="navbar navbar-light bg-light">
-  <span class="navbar-brand mb-0 h1">One More Drink!</span>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="index.php">One More Drink!</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="table.php">Record</a>
+      </li>
+    </ul>
+  </div>
 </nav>
 <br>
 
